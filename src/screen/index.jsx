948290e8e822f0d87486684b871d523b15da50a0
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-05-28 10:33:33 -0400

    definicion inicial de layout

---
+++ src/screen/index.jsx
@@ -0,0 +1,14 @@
+
+
+function Screen(){
+    return(
+        <div>{/* cuadro de la pantalla */}
+            <div></div>{/* info sala y reloj */}
+            <div></div>{/* tabla paciente X especialista */}
+            <div></div>{/* tabla paciente X llamado */}
+            <div></div>{/* info de tabla */}
+        </div>
+    )
+}
+
+export default Screen;
