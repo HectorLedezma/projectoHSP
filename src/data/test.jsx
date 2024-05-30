55351f4219cfeeabd673836dfa461276c6fce161
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-05-29 22:17:52 -0400

    pagina para probar consultas de datos

--- src/data/test.jsx
+++ src/data/test.jsx
@@ -1,22 +1,24 @@
+import { useRef, useState } from "react";
 import {Datos} from "./datos.js";
 
 function Test(){
 
     const datero = new Datos();
     //const data = datero.getDatos();
-
+    const [data,setData] = useState([]);
+    const idDepto = useRef();
     return(
         <div>
             <form onSubmit={ev=>{
                 ev.preventDefault();
-                datero.consultar();
+                setData(datero.consultar(Number(idDepto.current.value)))
             }}>
-            <input placeholder="Nombre profesional"/><br/>
-            <input placeholder="Departamento"/><br/>
-            <input placeholder="idDepartment"/><br/>
-
+            <input ref={idDepto} placeholder="idDepartment"/><br/>
             <button type="submit">Buscar</button>
             </form>
+            <div>
+                {JSON.stringify(data)}
+            </div>
         </div>
     )
 }
