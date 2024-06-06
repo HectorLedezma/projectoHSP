6c27951973aa45c5e9b0b6bd4978b320020f4fc6
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-06-06 02:53:16 -0400

    adaptacion a estructura de datos del hospital

--- src/classes/datos.js
+++ src/classes/datos.js
@@ -1,6 +1,7 @@
 import data from "../data/DataTest.json";
 import moduticket from "../data/modulos.json";
 import pantallas from "../data/Pantallas.json"
+import { ETL } from "./etl";
 export class Datos{
     fuente = data;
     modtick = moduticket;
@@ -11,7 +12,7 @@ export class Datos{
         let MyScreen = {
             "nombre":"",
             "boxs":[],
-
+            "mensaje":[]
         }
 
         //filtrar modulos
@@ -22,21 +23,66 @@ export class Datos{
             }
         }
         //filtrar tickets
-        let newTicks = [];
         for(let i = 0; i<this.modtick.tickets.length;i++){
-            if(this.modtick.tickets[i].idDepartment === idd){
-                newTicks.push(this.modtick.tickets[i]);
+    
+            if(this.modtick.tickets[i].idDepartment === idd && (this.modtick.tickets[i].estado !== 4 && this.modtick.tickets[i].estado !== 13)){
+                let newDato = {
+                    "name":this.modtick.tickets[i].nameModule,
+                    "dr":this.modtick.tickets[i].nombre_prof,
+                    "paciente":this.modtick.tickets[i].nombre_paciente,
+                    "estado":this.modtick.tickets[i].estado,
+                    "hora":this.modtick.tickets[i].hora_citacion
+                };
+                MyScreen.boxs.push(newDato);
+                
             }
+                
         }
         // get pantalla
-        let newScreen = {};
+        
         for(let i = 0; i<this.screens.length;i++){
             if(this.screens[i].pantalla.idDepartment === idd){
-                newScreen = this.screens[i];
+                MyScreen.nombre = this.screens[i].pantalla.nombre;
+                MyScreen.mensaje = this.screens[i].mensajes
                 break;
             }
         }
-        return JSON.stringify(newMods);
+
+
+        //MyScreen
+        let datos = MyScreen.boxs;
+        const etl = new ETL();
+        let doctors = [];
+        
+        let finalJSON = [];
+
+        for(let i = 0;i<datos.length;i++){//recorre el arreglo de datos proporcionado
+            if(!doctors.includes(datos[i].dr)){
+                doctors.push(datos[i].dr);
+                let subPatient = [];
+                let subPatientR = [];
+                console.log(datos[i].dr+":");
+                for(let j = 0;j<datos.length;j++){//recorre el arreglo de datos proporcionado otra vez
+                    if(datos[i].dr === datos[j].dr && (!subPatientR.includes(datos[j].paciente))){
+                        
+                        console.log("   "+datos[j].paciente);
+                        subPatientR.push(datos[j].paciente);
+                        subPatient.push({
+                            "Nombre":etl.recortaNombreP(datos[j].paciente),
+                            "Estado":datos[j].estado,
+                            "Hora":etl.getHora(datos[j].hora)
+                        });
+                    }
+                }
+                console.log("\n");
+                finalJSON.push({
+                    "medico":etl.recortaNombre(datos[i].dr),
+                    "box":datos[i].name,
+                    "pacientes":etl.PatientSort(subPatient)
+                })
+            }
+        }
+        return {"Name":MyScreen.nombre,"Datos":finalJSON,"Meseajes":MyScreen.mensaje}
     }
     
     consultar(idd){
