496ddefd5d35f3a1b0e21d575a9d57007d6f74fd
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-06-12 05:38:06 -0400

    componente de prueba de datos

--- src/data/test.jsx
+++ src/data/test.jsx
@@ -1,18 +1,20 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Datos } from "../classes/datos";
 
 
 function Test(){
-    const [res,setRes] = useState("");
-    const input = useRef();
+    const [res,setRes] = useState([]);
+    //const input = useRef();
     const dat = new Datos();
     useEffect(()=>{
-        dat.armaJSON2(1)
+        dat.armaJSON2(1);
     })
     return(
         <div>
             
-            <div className="bg-primary fs-1">{res}</div>
+            <div className="bg-primary fs-1">
+                {res}
+            </div>
             
             
         </div>
